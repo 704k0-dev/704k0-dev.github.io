4e621fd6eee7c47a03e485d4a3352fcf827f1ec9
Joaquín Suárez <elmusenda@gmail.com>
2025-02-06 15:43:18 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         .forEach(repo => {
                             let item = document.createElement('div');
                             let img = document.createElement('img');
-                            img.src = `https://raw.githubusercontent.com/704k0-dev/${repo.name}/main/thumbnail.png`;
+                            img.src = `https://raw.githubusercontent.com/704k0-dev/${repo.name}/main/thumbnail.jpg`;
                             img.onerror = function() { this.style.display = 'none'; };
                             item.innerHTML = `<h2><a href="${repo.html_url}" target="_blank">${repo.name.replace(/_/g, ' ')}</a></h2>
                                               <p>${repo.description || "Sin descripción"}</p>`;
